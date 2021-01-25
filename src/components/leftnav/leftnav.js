668112f67a63622f0da7a7ad75358d337b1d52be
Iván Flores <hivansan@gmail.com>
2021-01-25 02:03:02 -0600

    left nav to functional component

--- src/components/leftnav/leftnav.js
+++ src/components/leftnav/leftnav.js
@@ -1,58 +1,49 @@
+'use strict';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Store from '../../stores/store';
 import { FaThLarge } from 'react-icons/fa';
 
-const { store } = Store;
+// import Store from '../../stores/store';
+// const { store } = Store;
 
-class LeftNav extends React.Component {
-  constructor(props) {
-    super(props);
+const LeftNav = (props) => {
+  // const themeType = store.getStore('themeType');
+  // const activeclass = store.getStore('activeclass');
 
-    const themeType = store.getStore('themeType');
-    const activeclass = store.getStore('activeclass');
-    this.state = {
-      themeType: themeType,
-      activeclass: activeclass,
-    };
-  }
-
-  render() {
-    return (
-      <div className='leftNav'>
-        <ul>
-          <Link to='/#'>
-            <li className='item-menu'>
-              <FaThLarge />
-              <span className='menu'>Dashboard</span>
-            </li>
-          </Link>
-          <Link to='/hives'>
-            <li className='item-menu'>
-              <img alt='' src={require('../../assets/house.png')} width='20' />
-              <span className='menu'>Hive</span>
-            </li>
-          </Link>
-          <Link to='/farm'>
-            <li className='item-menu'>
-              <img alt='' src={require('../../assets/honey.png')} width='20' />
-              <span className='menu'>Farm</span>
-            </li>
-          </Link>
-          <Link to='/whaletank'>
-            <li className='item-menu'>
-              <img
-                alt=''
-                src={require('../../assets/whale-tail.png')}
-                width='20'
-              />
-              <span className='menu'>Whale Tank</span>
-            </li>
-          </Link>
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='leftNav'>
+      <ul>
+        <Link to='/#'>
+          <li className='item-menu'>
+            <FaThLarge />
+            <span className='menu'>Dashboard</span>
+          </li>
+        </Link>
+        <Link to='/hives'>
+          <li className='item-menu'>
+            <img alt='' src={require('../../assets/house.png')} width='20' />
+            <span className='menu'>Hive</span>
+          </li>
+        </Link>
+        <Link to='/farm'>
+          <li className='item-menu'>
+            <img alt='' src={require('../../assets/honey.png')} width='20' />
+            <span className='menu'>Farm</span>
+          </li>
+        </Link>
+        <Link to='/whaletank'>
+          <li className='item-menu'>
+            <img
+              alt=''
+              src={require('../../assets/whale-tail.png')}
+              width='20'
+            />
+            <span className='menu'>Whale Tank</span>
+          </li>
+        </Link>
+      </ul>
+    </div>
+  );
+};
 
 export default LeftNav;
