78ce4bbf41d4a314efbe566072042406b9c1c49f
Iván Flores <hivansan@gmail.com>
2021-02-06 23:44:06 -0600

    rewards mapper

---
+++ src/components/utils/rewardsMapper.js
@@ -0,0 +1,36 @@
+'use strict';
+const rewardsMapper = (rewards) => {
+  let result = rewards
+    .flatMap((rp) => rp.tokens[0])
+    .map((t) => {
+      return {
+        acronym: t.symbol,
+        name: t.name,
+        address: t.rewardsAddress,
+        inPool: t.inPool,
+        beastBonus: t.beastBonus || 0,
+        bonusReductionIn: t.bonusReductionIn || 0,
+        weeklyRewards: t.poolRatePerWeek,
+        myBeastModes: t.currentActiveBooster,
+        myRewards: t.rewardsAvailable,
+        symbol: t.symbol,
+        rewardsSymbol: t.rewardsSymbol,
+        stakedBalance: t.stakedBalance,
+        costBooster: t.costBooster,
+        costBoosterUSD: t.costBoosterUSD,
+        timeToNextBoost: t.timeToNextBoost,
+        currentBoosterStakeValue: t.currentBoosterStakeValue,
+        stakeValueNextBooster: t.stakeValueNextBooster,
+        liquidityLink: t.liquidityLink,
+        tokenAddress: t.tokenAddress,
+        tokenSymbol: t.tokenSymbol,
+        ethPrice: t.ethPrice,
+        boostBalance: t.boostBalance,
+        token: t,
+      };
+    });
+
+  return result;
+};
+
+export default rewardsMapper;
