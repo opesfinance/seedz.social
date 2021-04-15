031dd06b69a85d118bade0e9335802c19fdd8ea8
Iván Flores <hivansan@gmail.com>
2021-04-14 19:19:17 -0500

    header to function component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import UnlockModal from '../unlock/unlockModal.jsx';
@@ -13,123 +13,88 @@ import { Button } from 'react-bootstrap';
 
 import Store from '../../stores/store';
 const { emitter, store } = Store;
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    const account = store.getStore('account');
-    const themeType = store.getStore('themeType');
-    const activeClass = store.getStore('activeClass');
 
-    this.state = {
-      activeClass,
-      loading: false,
-      account: account,
-      assets: store.getStore('assets'),
-      modalOpen: false,
-      themeType: themeType,
+const Header = (props) => {
+  const [account, setAccount] = useState(store.getStore('account'));
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    emitter.on(ERROR, errorReturned);
+    emitter.on(CONNECTION_CONNECTED, connectionConnected);
+    emitter.on(CONNECTION_DISCONNECTED, connectionDisconnected);
+
+    return () => {
+      emitter.removeListener(ERROR, errorReturned);
+      emitter.removeListener(CONNECTION_CONNECTED, connectionConnected);
+      emitter.removeListener(CONNECTION_DISCONNECTED, connectionDisconnected);
     };
-  }
+  }, [props]);
 
-  componentDidMount() {
-    emitter.on(ERROR, this.errorReturned);
-    emitter.on(CONNECTION_CONNECTED, this.connectionConnected);
-    emitter.on(CONNECTION_DISCONNECTED, this.connectionDisconnected);
-    // emitter.on(CONFIGURE_RETURNED, this.configureReturned);
-  }
+  const connectionConnected = () => setAccount(store.getStore('account'));
 
-  componentWillUnmount() {
-    emitter.removeListener(ERROR, this.errorReturned);
-    emitter.removeListener(CONNECTION_CONNECTED, this.connectionConnected);
-    emitter.removeListener(
-      CONNECTION_DISCONNECTED,
-      this.connectionDisconnected
-    );
-    // emitter.removeListener(CONFIGURE_RETURNED, this.configureReturned);
-  }
+  const connectionDisconnected = () => setAccount(store.getStore('account'));
 
-  connectionConnected = () => {
-    this.setState({ account: store.getStore('account') });
-  };
-
-  configureReturned = () => {
-    //.this.props.history.push('/')
-  };
-
-  connectionDisconnected = () => {
-    this.setState({ account: store.getStore('account'), loading: false });
-  };
-
-  errorReturned = (error) => {
+  const errorReturned = (error) => {
     //TODO: handle errors
   };
 
-  renderModal = () => {
-    return (
-      <UnlockModal
-        closeModal={this.closeModal}
-        modalOpen={this.state.modalOpen}
-      />
-    );
+  const closeModal = () => setModalOpen(false);
+
+  const renderModal = (
+    <UnlockModal closeModal={closeModal} modalOpen={modalOpen} />
+  );
+
+  const unlockClicked = () => {
+    setModalOpen(true);
+    // this.setState({ modalOpen: true, loading: true });
   };
 
-  unlockClicked = () => {
-    this.setState({ modalOpen: true, loading: true });
-  };
+  const address = account?.address
+    ? account.address.substring(0, 6) +
+      '...' +
+      account.address.substring(
+        account.address.length - 4,
+        account.address.length
+      )
+    : null;
 
-  closeModal = () => {
-    this.setState({ modalOpen: false, loading: false });
-  };
+  return (
+    <>
+      <nav className='navbar navbar-expand-lg fixed-top'>
+        <Link to='/' className='navbar-brand'>
+          <img
+            className='navbar-logo'
+            src={require('../../assets/empty.png')}
+            height='30'
+            alt='Bees Social'
+          />
+        </Link>
 
-  render() {
-    const { account, modalOpen } = this.state;
-    var address = null;
-    if (account.address) {
-      address =
-        account.address.substring(0, 6) +
-        '...' +
-        account.address.substring(
-          account.address.length - 4,
-          account.address.length
-        );
-    }
-    return (
-      <>
-        <nav className='navbar navbar-expand-lg fixed-top'>
-          <Link to='/' className='navbar-brand'>
-            <img
-              className='navbar-logo'
-              src={require('../../assets/empty.png')}
-              height='30'
-              alt='Bees Social'
-            />
-          </Link>
+        <div>
+          {address && (
+            <Link to='/#' onClick={unlockClicked}>
+              <img
+                alt=''
+                src={require('../../assets/wallet-logo.png')}
+                height='30'
+              />
+              &nbsp;
+              <span className='text-purple'>{address}</span>
+            </Link>
+          )}
+          {!address && (
+            <Button className='btn btn-primary' onClick={unlockClicked}>
+              Connect Wallet
+            </Button>
+          )}
+        </div>
+        {/* <Navbar.Collapse className='justify-content-end'>
+        </Navbar.Collapse> */}
+      </nav>
 
-          <div>
-            {address && (
-              <Link to='/#' onClick={this.unlockClicked}>
-                <img
-                  alt=''
-                  src={require('../../assets/wallet-logo.png')}
-                  height='30'
-                />
-                &nbsp;
-                <span className='text-purple'>{address}</span>
-              </Link>
-            )}
-            {!address && (
-              <Button className='btn btn-primary' onClick={this.unlockClicked}>
-                Connect Wallet
-              </Button>
-            )}
-          </div>
-          {/* <Navbar.Collapse className='justify-content-end'>
-          </Navbar.Collapse> */}
-        </nav>
-
-        {modalOpen && this.renderModal()}
-      </>
-    );
-  }
-}
+      {modalOpen && renderModal}
+    </>
+  );
+};
 
 export default Header;
